05fe6727a98863a3f8b245d60a062a516a726c2f
Julien BONNETAUD <julien.bonnetaud@gmail.com>
2014-11-23 00:58:37 +0100

    README update

---
+++ src/ArrayValidator.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Razor
+ * Date: 23/11/14
+ * Time: 00:55
+ */
+
+namespace JB\Validator;
+
+
+class ArrayValidator
+{
+    public static function isEmpty(){
+
+    }
+    public static function numberElements(){
+
+    }
+    public static function getElementsBetween(){
+
+    }
+    public static function keyExists(){
+
+    }
+    public static function valueExists(){
+
+    }
+} 
---
+++ src/DateTimeValidator.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Razor
+ * Date: 23/11/14
+ * Time: 00:52
+ */
+
+namespace JB\Validator;
+
+
+class DateTimeValidator
+{
+    public static function isMajor(){
+
+    }
+
+    public static function isYear(){
+
+    }
+    public static function getTimeTo(){
+
+    }
+} 
