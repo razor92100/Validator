58053ea46d6c6e42613a34f480656be23a01796a
Julien BONNETAUD <julien.bonnetaud@gmail.com>
2014-11-23 19:23:47 +0100

    ArrayValidator update

--- src/ArrayValidator.php
+++ src/ArrayValidator.php
@@ -9,13 +9,30 @@
 namespace JB\Validator;
 
 
+/**
+ * Class ArrayValidator
+ * @package JB\Validator
+ */
 class ArrayValidator
 {
+    /**
+     * @param array $array
+     * @return bool
+     */
     public static function isEmpty($array){
-
+        if (empty($array)){
+            return true;
+        }else {
+            return false;
+        }
     }
-    public static function numberElements($array){
 
+    /**
+     * @param array $array
+     */
+    public static function numberElements($array){
+        $nbElements = array_count_values($array);
+        echo $nbElements;
     }
     public static function getElementsBetween($array, $from, $to){
 
