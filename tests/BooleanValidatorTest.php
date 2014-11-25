a1d49fc1c83b44c384b3a0b3db38d967d25b2ff3
Julien BONNETAUD <julien.bonnetaud@gmail.com>
2014-11-25 11:41:57 +0100

    Tests Boolean and Integer added

--- tests/BooleanValidatorTest.php
+++ tests/BooleanValidatorTest.php
@@ -10,7 +10,31 @@ namespace Tests\JB\Validator;
 
 use JB\Validator;
 
+/**
+ * Class BooleanValidatorTest
+ * @package Tests\JB\Validator
+ */
 class BooleanValidatorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     *
+     */
+    public function testIsTrue()
+    {
+        $boolean = true;
+        $test = Validator\BooleanValidator::isTrue($boolean);
 
+        $this->assertEquals($boolean, $test);
+    }
+
+    /**
+     *
+     */
+    public function testIsFalse()
+    {
+        $boolean = false;
+        $test = Validator\BooleanValidator::isFalse($boolean);
+
+        $this->assertEquals(true, $test);
+    }
 } 
--- tests/IntegerValidatorTest.php
+++ tests/IntegerValidatorTest.php
@@ -10,7 +10,90 @@ namespace Tests\JB\Validator;
 
 use JB\Validator;
 
+/**
+ * Class IntegerValidatorTest
+ * @package Tests\JB\Validator
+ */
 class IntegerValidatorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     *
+     */
+    public function testIsEqual()
+    {
+        $a = $b = 2;
+        $expected = true;
+        $test = Validator\IntegerValidator::isEqual($a, $b);
 
+        $this->assertEquals($expected, $test);
+    }
+
+    /**
+     *
+     */
+    public function testIsSuperior()
+    {
+        $a = 10;
+        $b = 5;
+        $expected = true;
+
+        $test = Validator\IntegerValidator::isSuperior($a, $b);
+
+        $this->assertEquals($expected, $test);
+    }
+
+    /**
+     *
+     */
+    public function  testIsInferior()
+    {
+        $a = 5;
+        $b = 10;
+        $expected = true;
+
+        $test = Validator\IntegerValidator::isInferior($a, $b);
+
+        $this->assertEquals($expected, $test);
+    }
+
+    /**
+     *
+     */
+    public function testIsBetween()
+    {
+        $min = 1;
+        $max = 10;
+        $nb = 5;
+        $expected = true;
+
+        $test = Validator\IntegerValidator::isBetween($nb, $min, $max);
+
+        $this->assertEquals($expected, $test);
+    }
+
+    /**
+     *
+     */
+    public function testIsNegative()
+    {
+        $nb = -5;
+        $expected = true;
+
+        $test = Validator\IntegerValidator::isNegative($nb);
+
+        $this->assertEquals($expected, $test);
+    }
+
+    /**
+     *
+     */
+    public function testIsPositive()
+    {
+        $nb = 5;
+        $expected = true;
+
+        $test = Validator\IntegerValidator::isPositive($nb);
+
+        $this->assertEquals($expected, $test);
+    }
 } 
